46d0d9b49f5c049924253b79b78c0b14eba45502
EmmanuelDanso1 <kwekudanso21@gmail.com>
2024-10-11 10:28:25 -0700

    student loc

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,10 @@
+/*   location: String
+ * }[]} students - The list of students.
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
